4572eb56b120b653e907f645288e31470a9ec5a1
Omar Angelino <omar.angelino@streamlabs.com>
2018-10-22 10:14:00 -0700

    init

---
+++ repo-test.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+
+class test {
+
+	 constructor() {
+	}
+
+	printName(name, lastname)
+	{
+		console.log(`${name} ${lastname}`);
+	}
+
+}
+
+module.export.default = new test;
